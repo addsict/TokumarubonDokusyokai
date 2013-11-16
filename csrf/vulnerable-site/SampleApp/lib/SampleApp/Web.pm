7710c42e206e7130f3534f3292d0789c673b6fc1
Yuuki Furuyama <addsict@gmail.com>
2013-11-16 22:31:33 +0900

    tweak

--- csrf/vulnerable-site/SampleApp/lib/SampleApp/Web.pm
+++ csrf/vulnerable-site/SampleApp/lib/SampleApp/Web.pm
@@ -5,6 +5,7 @@ use warnings;
 use utf8;
 use Kossy;
 use Log::Minimal;
+use Data::Dumper;
 
 use constant VALID_USERNAME => 'taro';
 use constant VALID_PASSWORD => 'pass';
@@ -13,6 +14,17 @@ sub is_valid_account {
     my ($username, $password) = @_;
     return 1 if $username eq VALID_USERNAME && $password eq VALID_PASSWORD;
     return;
+}
+
+sub create_entry {
+    my ($title, $body) = @_;
+    my ($sec, $min, $hour, $day, $month, $year) = localtime(time);
+    my $date = sprintf("%d-%d-%d %d:%d:%d", $year+1990, $month+1, $day, $hour, $min, $sec);
+    return {
+        date => $date,
+        title => $title,
+        body => $body,
+    };
 }
 
 get '/' => sub {
@@ -28,27 +40,22 @@ get '/' => sub {
 post '/' => sub {
     my ( $self, $c )  = @_;
     my $session = $c->env->{'psgix.session'};
+
+    # show request header
+    for my $header (keys %{$c->req->headers}) {
+        debugf $header . ": " . $c->req->headers->{$header};
+    }
+
+    # fetch request body
     my $result = $c->req->validator([
-        'title' => {
-            rule => [
-                ['NOT_NULL', '']
-            ],
-        },
-        'body' => {
-            rule => [
-                ['NOT_NULL', '']
-            ],
-        },
+        'title' => { rule => [['NOT_NULL', '']], },
+        'body' => { rule => [['NOT_NULL', '']], },
     ]);
     my $title = $result->valid('title');
     my $body = $result->valid('body');
-    my ($sec, $min, $hour, $day, $month, $year) = localtime(time);
-    my $date = sprintf("%d-%d-%d %d:%d:%d", $year+1990, $month+1, $day, $hour, $min, $sec);
-    my $entry = {
-        date => $date,
-        title => $title,
-        body => $body,
-    };
+
+    my $entry = create_entry($title, $body);
+
     if ($session->{entries}) {
         push $session->{entries}, $entry;
     } else {
@@ -66,22 +73,11 @@ post '/login' => sub {
     my ( $self, $c )  = @_;
     
     my $result = $c->req->validator([
-        'username' => {
-            rule => [
-                ['NOT_NULL', '']
-            ],
-        },
-        'password' => {
-            rule => [
-                ['NOT_NULL', '']
-            ],
-        },
+        'username' => { rule => [['NOT_NULL', '']], },
+        'password' => { rule => [['NOT_NULL', '']], },
     ]);
-
     my $username = $result->valid('username');
     my $password = $result->valid('password');
-    debugf($username);
-    debugf($password);
 
     if ($result->has_error || !is_valid_account($username, $password)) {
         # login fail
